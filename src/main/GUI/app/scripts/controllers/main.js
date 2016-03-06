6efcc1b6301dee984f34a463763e478113ec93be
mathieu.rolland14270@orange.fr <mathieu.rolland14270@orange.fr>
2016-03-06 20:03:55 +0100

    Create angular project

---
+++ src/main/GUI/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mediathequeUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mediathequeUiApp
+ */
+angular.module('mediathequeUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
