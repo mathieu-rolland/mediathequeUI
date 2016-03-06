5d0093a7c24c7d62520684d37dfc577e7f721bb0
mathieu.rolland14270@orange.fr <mathieu.rolland14270@orange.fr>
2016-03-06 19:30:01 +0100

    Create and fixe service movies url

--- src/main/java/com/perso/service/MoviesService.java
+++ src/main/java/com/perso/service/MoviesService.java
@@ -4,20 +4,22 @@ import java.io.UnsupportedEncodingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.allocine.IAllocineAPI;
 
 @RestController
+@RequestMapping("/movies")
 public class MoviesService {
 
 	@Autowired
 	private IAllocineAPI api;
 	
-	@RequestMapping("/")
-	public String printMovie(){
+	@RequestMapping("/search")
+	public String searchMovie(@RequestParam(value="q", defaultValue="test") String search){
 		try {
-			api.searchMovies("Harry potter");
+			return api.searchMovies( search ).getFeed().toString();
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
