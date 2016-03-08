57e816b182f378e900ba5bcee45d6a61326b1eb1
mathieu.rolland14270@orange.fr <mathieu.rolland14270@orange.fr>
2016-03-08 22:21:35 +0100

    Fix database access using application.properties

--- src/main/java/com/perso/launcher/Launcher.java
+++ src/main/java/com/perso/launcher/Launcher.java
@@ -1,17 +1,21 @@
 package com.perso.launcher;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 @ComponentScan({"com.perso.service", "com.perso.config"})
-@ImportResource("classpath:config.xml")
+@ImportResource({"classpath:config.xml"})
 @SpringBootApplication
 public class Launcher {
 
+	static Logger log = Logger.getLogger(Launcher.class.getName());
+	
 	public static void main(String[] args) {
-		 SpringApplication.run(Launcher.class, args);
+		SpringApplication.run(Launcher.class, args);
+		log.info( "Start application successfully" );
 	}
 	
 }
