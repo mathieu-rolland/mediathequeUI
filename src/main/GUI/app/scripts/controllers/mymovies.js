2b6c24c86789ae3a034af05af794241050aaf312
mathieu.rolland14270@orange.fr <mathieu.rolland14270@orange.fr>
2016-07-16 15:11:50 +0200

    Filter and sort mediatheque movie

--- src/main/GUI/app/scripts/controllers/mymovies.js
+++ src/main/GUI/app/scripts/controllers/mymovies.js
@@ -7,7 +7,7 @@
  * # MymoviesCtrl
  * Controller of the mediathequeUiApp
  */
-angular.module('mediathequeUiApp').controller('MymoviesCtrl',[ '$scope' , 'AllocineWebService' , function ( $scope , AllocineWebService) {
+angular.module('mediathequeUiApp').controller('MymoviesCtrl',[ '$scope' , 'AllocineWebService' , 'localStorageService' , function ( $scope , AllocineWebService, localStorageService) {
 	
 	var myMovieCallback = function(response){
 		console.log(response);
@@ -18,8 +18,30 @@ angular.module('mediathequeUiApp').controller('MymoviesCtrl',[ '$scope' , 'Alloc
 	
 	AllocineWebService.loadMyMovie( myMovieCallback );
 	
-	$scope.formatDuration = function( duration ){
-		"rien"
+	$scope.changeSort = function( sortBy ){
+		
+		localStorageService.set('my-movie-sort' , sortBy );
+		$scope.sortKey=sortBy;
+		
+		switch( sortBy ){
+			case 'title':
+				$scope.sortBy='Titre';
+				break;
+			case 'year' :
+				$scope.sortBy='Date de sortie';
+				break;
+			default:
+				$scope.sortKey='title';
+				$scope.sortBy='Titre';
+				break;
+		}
+		
+	};
+	
+	var previousSort = localStorageService.get('my-movie-sort');
+	if( angular.isDefined(previousSort) ){
+		$scope.changeSort( previousSort );
 	}
+	
 	
 }]);
