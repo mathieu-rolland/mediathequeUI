4e841beccae092d7b1ed8be4af0ff2dcdd064969
Mathieu <mathieu.rolland2705@gmail.com>
2017-03-14 22:42:16 +0100

    Send email on user inscription

---
+++ src/main/GUI/app/scripts/admin/controllers/manageusers.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mediathequeUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mediathequeUiApp
+ */
+angular.module('mediathequeUiApp')
+  .controller('ManageUserCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
